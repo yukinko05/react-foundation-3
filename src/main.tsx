c8b28fdea87eaebee79a386b8132c5dff0ef443a
yukinko05 <grey.sora.35@gmail.com>
2024-09-15 05:37:08 +0900

    React Routerでルーティング処理を実装

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '@unocss/reset/eric-meyer.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')! as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
