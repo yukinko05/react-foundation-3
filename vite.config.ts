f4637dd0388eb6e5f10607ff3496a8bfbf94fcd5
yukinko05 <grey.sora.35@gmail.com>
2024-09-15 18:54:48 +0900

    Vitestのインストールおよび設定
    - Playwrightのテストを実行する参照先の修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    // テストに関するAPIをグローバルに設定
+    globals: true,
+    // テスト環境の設定
+    environment: 'jsdom',
+    // テストの設定ファイル
+    setupFiles: './tests/unit/setup.ts', // CSSファイルを処理する
+    css: true,
+    // テストのカバレッジを出力する設定
+    coverage: {
+      // @vitest/coverage-v8を設定
+      provider: 'v8',
+      exclude: [...(configDefaults.coverage.exclude as string[]), 'src/test', 'src/main.tsx'],
+    },
+  },
+});
