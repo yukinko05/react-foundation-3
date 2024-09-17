c455005b781cafb210ee95a48f36f1706ca9de29
yukinko05 <grey.sora.35@gmail.com>
2024-09-17 16:43:21 +0900

    新規登録画面のUIを実装

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,20 @@
 export default function SignUp() {
   return (
-    <></>
-  )
-};
+    <>
+      <h1>新規登録</h1>
+      <form>
+        <div>
+          <label htmlFor="email">メールアドレス</label>
+          <input type="email" id="email" name="email" />
+        </div>
+
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input type="password" id="password" name="password" />
+        </div>
+
+        <button>作成</button>
+      </form>
+    </>
+  );
+}
