fec80f712058812683d7fabcd4eae936d681779e
yukinko05 <grey.sora.35@gmail.com>
2024-09-17 17:16:32 +0900

    新規登録画面のバリデーションを実装
    - HTMLの標準バリデーションチェックは不要なためnovalidate属性を追加

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -20,15 +20,29 @@ const SignUp: FC = () => {
   return (
     <>
       <h1>新規登録</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <div>
           <label htmlFor="email">メールアドレス</label>
-          <input {...register("email")} type="email" id="email" name="email" />
+          <input {...register("email", {
+            required: 'メールアドレスは必須です',
+            pattern: {
+              value: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/,
+              message: '正しいメールアドレスを入力してください',
+            },
+          })} type="email" id="email" name="email" />
+          {errors.email && <p>{errors.email.message}</p>}
         </div>
 
         <div>
           <label htmlFor="password">パスワード</label>
-          <input {...register("password")} type="password" id="password" name="password" />
+          <input {...register("password", {
+            required: 'パスワードは必須です',
+            minLength: {
+              value: 8,
+              message: 'パスワードは8文字以上で入力してください',
+            },
+          })} type="password" id="password" name="password" />
+          {errors.password && <p>{errors.password.message}</p>}
         </div>
 
         <button>作成</button>
