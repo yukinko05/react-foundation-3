858694aec1aa26175e4791ea169d767f5e6aeb6a
yukinko05 <grey.sora.35@gmail.com>
2024-09-18 18:02:41 +0900

    ログイン画面にタイトルを追加してUIの整理

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -18,38 +18,46 @@ const SignIn: FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="email">メールアドレス</label>
-      <input
-        {...register('email', {
-          required: 'メールアドレスは必須です',
-          pattern: {
-            value: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/,
-            message: '正しいメールアドレスを入力してください',
-          },
-        })}
-        type="email"
-        id="email"
-        name="email"
-      />
-      {errors.email && <p>{errors.email.message}</p>}
+    <>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="email">メールアドレス</label>
+          <input
+            {...register('email', {
+              required: 'メールアドレスは必須です',
+              pattern: {
+                value: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/,
+                message: '正しいメールアドレスを入力してください',
+              },
+            })}
+            type="email"
+            id="email"
+            name="email"
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
 
-      <label htmlFor="password">パスワード</label>
-      <input
-        {...register('password', {
-          required: 'パスワードは必須です',
-          minLength: {
-            value: 8,
-            message: 'パスワードは8文字以上で入力してください',
-          },
-        })}
-        type="password"
-        id="password"
-        name="password"
-      />
-      {errors.password && <p>{errors.password.message}</p>}
-      <button type="submit">ログイン</button>
-    </form>
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input
+            {...register('password', {
+              required: 'パスワードは必須です',
+              minLength: {
+                value: 8,
+                message: 'パスワードは8文字以上で入力してください',
+              },
+            })}
+            type="password"
+            id="password"
+            name="password"
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
+
+        <button type="submit">ログイン</button>
+      </form>
+    </>
   );
 };
 
