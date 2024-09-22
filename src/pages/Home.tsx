4fb08308bafb485c0dfd486027a66e764723c829
yukinko05 <grey.sora.35@gmail.com>
2024-09-22 17:51:08 +0900

    書籍レビューデータを10件取得するためにGET/public/booksを実行

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await axios.get('https://railway.bookreview.techtrain.dev/public/books');
+      setBooks(response.data);
+    }
+
+    fetchBooks();
+  }, []);
+
+  console.log(books);
+
   return (
     <div>
       <h1>Home</h1>
