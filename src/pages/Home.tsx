18d2452ddffc443f6033c114f361ca0c1bd2ecb1
yukinko05 <grey.sora.35@gmail.com>
2024-09-23 18:25:51 +0900

    書籍レビューの一覧画面のStyle調整

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,19 +22,21 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Book Reviews</h1>
-      <ul>
-        {books.map((book: Book) => (
-          <li key={book.id}>
-            <p>{book.title}</p>
-            <p>{book.url}</p>
-            <p>{book.reviewer}</p>
-            <p>{book.review}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <main className="flex min-h-full flex-col px-6 py-12 lg:px-8">
+      <h1 className=" text-center text-4xl text-gray-900">Book Reviews</h1>
+      <div className='mt-10'>
+        <ul className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
+          {books.map((book: Book) => (
+            <li key={book.id} className='border border-gray-300 rounded-md'>
+              <p className='px-2 pt-3 text-xl text-gray-900'>{book.title}</p>
+              <p className='p-2 text-sm text-gray-600 border-b-2 border-gray-200'>URL：<a href={book.url}>{book.url}</a></p>
+              <p className='px-2 pt-3 text-sm text-gray-900'>レビュワー：{book.reviewer}</p>
+              <p className='px-2 py-1 text-sm text-gray-900'>{book.review}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 };
 
