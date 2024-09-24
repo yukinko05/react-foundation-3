c3432bd6099625b321ef36252cf1e0ee4a952908
yukinko05 <grey.sora.35@gmail.com>
2024-09-24 19:00:27 +0900

    offsetのSlice構築

---
+++ src/offsetSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface OffsetState {
+  offset: number;
+}
+
+const initialState: OffsetState = {
+  offset: 0,
+};
+
+export const offsetSlice = createSlice({
+  name: 'offset',
+  initialState,
+  reducers: {
+    nextPage: (state) => {
+      state.offset += 10;
+    },
+    previousPage: (state) => {
+      state.offset = Math.max(0, state.offset - 10);
+    },
+  },
+});
+
+export const { nextPage, previousPage } = offsetSlice.actions;
+export default offsetSlice.reducer;
