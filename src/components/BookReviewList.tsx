3c45b61fb1b2d9e968865d2ad6b5ecd979c311d0
yukinko05 <grey.sora.35@gmail.com>
2024-09-25 18:47:46 +0900

    書籍レビューカードのコンポーネント作成

--- src/components/BookReviewList.tsx
+++ src/components/BookReviewList.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { RootState } from './store';
 import { Book } from './types';
+import ReviewCard from './ReviewCard';
 
 const BookReviewList = () => {
   const offset = useSelector((state: RootState) => state.offset.offset);
@@ -21,14 +22,7 @@ const BookReviewList = () => {
   return (
     <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2">
       {bookReviews.map((book: Book) => (
-        <li key={book.id} className="border border-gray-300 rounded-md">
-          <p className="px-2 pt-3 text-xl text-gray-900">{book.title}</p>
-          <p className="p-2 text-sm text-gray-600 border-b-2 border-gray-200">
-            URL：<a href={book.url}>{book.url}</a>
-          </p>
-          <p className="px-2 pt-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
-          <p className="px-2 py-1 text-sm text-gray-900">{book.review}</p>
-        </li>
+        <ReviewCard key={book.id} book={book} />
       ))}
     </ul>
   );
