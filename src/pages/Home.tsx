4aecb6ece5bd8a2b65142f8a126d41ef8674ab4c
yukinko05 <grey.sora.35@gmail.com>
2024-09-26 16:38:58 +0900

    ページネーションボタンのStyle調整

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,19 @@ const Home = () => {
     <main className="flex min-h-full flex-col px-6 py-12 lg:px-8">
       <h1 className=" text-center text-4xl text-gray-900">Book Reviews</h1>
       <div className="mt-10">
-        <button onClick={() => dispatch(nextPage())}>次へ</button>
-        <button onClick={() => dispatch(previousPage())}> 前へ</button>
+        <button
+          onClick={() => dispatch(previousPage())}
+          className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-4 rounded-md"
+        >
+          {' '}
+          前へ
+        </button>
+        <button
+          onClick={() => dispatch(nextPage())}
+          className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-4 ml-2 rounded-md"
+        >
+          次へ
+        </button>
         <BookReviewList />
       </div>
     </main>
