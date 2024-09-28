087ae7d7c591affa1168bcc028d8155d69d2bff9
yukinko05 <grey.sora.35@gmail.com>
2024-09-28 23:27:39 +0900

    vitestのテスト修正

--- tests/unit/login.test.tsx
+++ tests/unit/login.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
-import SignIn from '../../src/pages/SignIn';
-
+import SignIn from '../../src/features/auth/SignIn';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('ログイン画面コンポーネント', () => {
   it('メールアドレスとパスワードの入力フォームが表示されるログイン画面のレンダリング', () => {
-    render(<SignIn />);
+    render(<BrowserRouter><SignIn /></BrowserRouter>);
 
-    const emailInput = screen.getByLabelText('メールアドレス');
+    const emailInput = screen.getByLabelText(/メールアドレス/i);
     expect(emailInput).toBeInTheDocument();
 
-    const passwordInput = screen.getByLabelText('パスワード');
+    const passwordInput = screen.getByLabelText(/パスワード/i);
     expect(passwordInput).toBeInTheDocument();
 
     const loginButton = screen.getByRole('button', { name: 'ログイン' });
@@ -19,3 +19,5 @@ describe('ログイン画面コンポーネント', () => {
 
   });
 });
+
+console.log(screen.debug());
