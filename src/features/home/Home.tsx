0461ecfe85a7b18223e48decb3426ea7c5b77d6b
yukinko05 <grey.sora.35@gmail.com>
2024-09-28 13:48:55 +0900

    ヘッダーコンポーネント作成

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,11 +1,13 @@
 import BookReviewList from '../bookReviews/BookReviewList';
 import { nextPage, previousPage } from '../bookReviews/offsetSlice';
 import { useDispatch } from 'react-redux';
+import Header from '../../components/Header';
+
 const Home = () => {
   const dispatch = useDispatch();
   return (
     <main className="flex min-h-full flex-col px-6 py-12 lg:px-8">
-      <h1 className=" text-center text-4xl text-gray-900">Book Reviews</h1>
+      <Header />
       <div className="mt-10">
         <button
           onClick={() => dispatch(previousPage())}
