cabd82f9b481e9a57595e924b5fac4ac6109ad52
yukinko05 <grey.sora.35@gmail.com>
2024-09-28 20:10:38 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/react-foundation-3' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/react-foundation-3' : '/',
   plugins: [react()],
   test: {
     // テストに関するAPIをグローバルに設定
