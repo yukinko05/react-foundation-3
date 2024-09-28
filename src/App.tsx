7092f6ed690a0af33b1302819fa3ce55a2db2f11
yukinko05 <grey.sora.35@gmail.com>
2024-09-28 22:32:21 +0900

    開発と本番モードでのルーティングを設定

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RoutesComponent from './routes';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-foundation-3/"}>
       <RoutesComponent />
     </BrowserRouter>
   );
