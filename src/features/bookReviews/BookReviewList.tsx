8213695e60e9c61d30ca4dc2f071d5f87af1dbf8
yukinko05 <grey.sora.35@gmail.com>
2024-09-29 03:45:31 +0900

    ログインしているユーザーのみが書籍レビュー情報を取得できるように変更
    - データ取得できなかった場合のエラーハンドリングを実装

--- src/features/bookReviews/BookReviewList.tsx
+++ src/features/bookReviews/BookReviewList.tsx
@@ -4,27 +4,42 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
 import { Book } from '../../types';
 import ReviewCard from './ReviewCard';
+import { useCookies } from 'react-cookie';
 
 const BookReviewList = () => {
   const offset = useSelector((state: RootState) => state.offset.offset);
   const [bookReviews, setBookReviews] = useState<Book[]>([]);
+  const [cookies] = useCookies(['token']);
+  const token = cookies.token;
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchBooks = async () => {
-      const response = await axios.get(
-        `https://railway.bookreview.techtrain.dev/public/books?offset=${offset}`
-      );
-      setBookReviews(response.data);
+      try {
+        const response = await axios.get(
+          `https://railway.bookreview.techtrain.dev/books?offset=${offset}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          }
+        }
+        );
+        setBookReviews(response.data);
+      } catch (error) {
+        setErrorMessage(`ログインに失敗しました。${error}`);
+      }
     };
     fetchBooks();
   }, [offset]);
 
   return (
-    <ul className="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2">
-      {bookReviews.map((book: Book) => (
-        <ReviewCard key={book.id} book={book} />
-      ))}
-    </ul>
+    <div>
+      {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+      <ul className="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2">
+        {bookReviews.map((book: Book) => (
+          <ReviewCard key={book.id} book={book} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
