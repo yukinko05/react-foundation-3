8425f7867a3803c44503d20e310df531f66467e5
yukinko05 <grey.sora.35@gmail.com>
2024-10-05 23:07:15 +0900

    ユーザー情報をReduxで管理

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import offsetReducer from '../features/bookReviews/offsetSlice';
 import authReducer from '../pages/auth/authSlice';
+import userReducer from '../features/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
     offset: offsetReducer,
     auth: authReducer,
+    user: userReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
