0743745cee9fb8b4a85c5b713f4e0eb2c4ceb48e
yukinko05 <grey.sora.35@gmail.com>
2024-10-12 22:41:14 +0900

    書籍レビューCardのstyle調整

--- src/features/bookReviews/ReviewCard.tsx
+++ src/features/bookReviews/ReviewCard.tsx
@@ -8,16 +8,18 @@ const ReviewCard = (props: ReviewCardProps) => {
   const { book } = props;
 
   return (
-    <li className="border border-gray-300 rounded-md">
-      <p className="px-2 pt-3 text-xl text-gray-900">{book.title}</p>
-      <p className="p-2 text-sm text-gray-600 border-b-2 border-gray-200">
-        URL：
-        <a href={book.url} target="_blank">
-          {book.url}
-        </a>
+    <li className="border border-gray-300 rounded-md pt-2 px-1">
+      <a
+        href={book.url}
+        target="_blank"
+        className="px-2 text-xl text-gray-900 hover:text-blue-600 transition-colors duration-300 inline-block border-b-2 border-transparent hover:border-blue-600"
+      >
+        {book.title}
+      </a>
+      <p className="px-2 pt-2 text-sm mt-1 text-gray-900 border-t-2 border-gray-200">
+        {book.detail}
       </p>
-      <p className="px-2 pt-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
-      <p className="px-2 py-1 text-sm text-gray-900">{book.detail}</p>
+      <p className=" px-2 py-3 text-sm text-gray-900 ">レビュワー：{book.reviewer}</p>
     </li>
   );
 };
