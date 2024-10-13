5cac045e553ee298f339058b7201aaa3708196f4
yukinko05 <grey.sora.35@gmail.com>
2024-10-13 18:32:14 +0900

    aタグからLinkタグに修正

--- src/features/bookReviews/ReviewCard.tsx
+++ src/features/bookReviews/ReviewCard.tsx
@@ -1,4 +1,5 @@
 import { Book } from '../../types';
+import { Link } from 'react-router-dom';
 
 interface ReviewCardProps {
   book: Book;
@@ -10,13 +11,13 @@ const ReviewCard = (props: ReviewCardProps) => {
   return (
     <li className="border border-gray-300 rounded-md pt-2 px-2">
       <div className="border-b border-gray-300 py-2">
-        <a
-          href={book.url}
+        <Link
+          to={book.url}
           target="_blank"
           className="text-xl text-gray-900 line-clamp-2 hover:text-blue-600 duration-300"
         >
           {book.title}
-        </a>
+        </Link>
       </div>
       <p className="px-2 pt-2 text-sm mt-1 text-gray-900 truncate">{book.detail}</p>
       <p className=" px-2 py-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
