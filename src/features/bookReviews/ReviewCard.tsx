7048ad6451ff98ffd9dd90fd888ba4bd95881b91
yukinko05 <grey.sora.35@gmail.com>
2024-10-13 18:12:42 +0900

    タイトルが２行以上、詳細が一行以降の場合は「...」と表示されるようにstyle調整

--- src/features/bookReviews/ReviewCard.tsx
+++ src/features/bookReviews/ReviewCard.tsx
@@ -8,18 +8,18 @@ const ReviewCard = (props: ReviewCardProps) => {
   const { book } = props;
 
   return (
-    <li className="border border-gray-300 rounded-md pt-2 px-1">
-      <a
-        href={book.url}
-        target="_blank"
-        className="px-2 text-xl text-gray-900 hover:text-blue-600 transition-colors duration-300 inline-block border-b-2 border-transparent hover:border-blue-600"
-      >
-        {book.title}
-      </a>
-      <p className="px-2 pt-2 text-sm mt-1 text-gray-900 border-t-2 border-gray-200">
-        {book.detail}
-      </p>
-      <p className=" px-2 py-3 text-sm text-gray-900 ">レビュワー：{book.reviewer}</p>
+    <li className="border border-gray-300 rounded-md pt-2 px-2">
+      <div className="border-b border-gray-300 py-2">
+        <a
+          href={book.url}
+          target="_blank"
+          className="text-xl text-gray-900 line-clamp-2 hover:text-blue-600 duration-300"
+        >
+          {book.title}
+        </a>
+      </div>
+      <p className="px-2 pt-2 text-sm mt-1 text-gray-900 truncate">{book.detail}</p>
+      <p className=" px-2 py-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
     </li>
   );
 };
