9073e1476bf19665a1aeaa574bdfcf4a14bc9f0b
yukinko05 <grey.sora.35@gmail.com>
2024-10-15 20:36:36 +0900

    書籍レビューカードをクリックすると詳細画面に遷移するよう実装

--- src/features/bookReviews/ReviewCard.tsx
+++ src/features/bookReviews/ReviewCard.tsx
@@ -1,6 +1,5 @@
 import { Book } from '../../types';
 import { Link } from 'react-router-dom';
-
 interface ReviewCardProps {
   book: Book;
 }
@@ -9,20 +8,14 @@ const ReviewCard = (props: ReviewCardProps) => {
   const { book } = props;
 
   return (
-    <li className="border border-gray-300 rounded-md pt-2 px-2">
-      <div className="border-b border-gray-300 py-2">
-        <div className="inline-block hover:border-b hover:border-blue-600">
-          <Link
-            to={book.url}
-            target="_blank"
-            className="text-xl text-gray-900 line-clamp-2 hover:text-blue-600 duration-300"
-          >
-            {book.title}
-          </Link>
+    <li className="border border-gray-300 rounded-md pt-2 px-2 hover:opacity-50">
+      <Link to={`/detail/${book.id}`} target="_blank" className="text-xl text-gray-900">
+        <div className="border-b border-gray-300 py-2">
+          <p className="line-clamp-2">{book.title}</p>
         </div>
-      </div>
-      <p className="px-2 pt-2 text-sm mt-1 text-gray-900 truncate">{book.detail}</p>
-      <p className=" px-2 py-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
+        <p className=" px-2 py-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
+        <p className="px-2 pt-2 text-sm mt-1 text-gray-900 truncate">{book.review}</p>
+      </Link>
     </li>
   );
 };
