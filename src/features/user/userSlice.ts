46cec482496e95d5893c8cc590c866203397f5b0
yukinko05 <grey.sora.35@gmail.com>
2024-10-19 20:32:54 +0900

    ユーザーの認証状態をuserSliceに追加する

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -4,7 +4,7 @@ import { User } from '../../types';
 
 export const fetchUserData = createAsyncThunk<User, string>(
   'users/fetchUserData',
-  async (token: string) => {
+  async (token: string, thunkAPI) => {
     try {
       const response = await axios.get('https://railway.bookreview.techtrain.dev/users', {
         headers: {
@@ -14,38 +14,59 @@ export const fetchUserData = createAsyncThunk<User, string>(
       return response.data;
     } catch (error) {
       if (error instanceof AxiosError && error.response && error.response.data) {
+        if (error.response.status === 401) {
+          return thunkAPI.rejectWithValue('Unauthorized');
+        }
+
         const errorMessage = error.response.data.ErrorMessageJP || 'エラーが発生しました。';
-        throw new Error(errorMessage);
+        return thunkAPI.rejectWithValue(errorMessage);
       }
-      throw new Error('ネットワークエラーが発生しました。');
+      return thunkAPI.rejectWithValue('ネットワークエラーが発生しました。');
     }
   }
 );
 
 interface userState {
   userData: User | null;
+  isAuthenticated: boolean | null;
   errorMessage: string | undefined;
 }
 
 const initialState: userState = {
   userData: null,
+  isAuthenticated: null,
   errorMessage: undefined,
 };
 
 const userSlice = createSlice({
   name: 'userData',
   initialState,
-  reducers: {},
+  reducers: {
+    signin: (state) => {
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchUserData.fulfilled, (state, action) => {
       state.userData = action.payload;
+      state.isAuthenticated = true;
       state.errorMessage = undefined;
     });
     builder.addCase(fetchUserData.rejected, (state, action) => {
       state.userData = null;
-      state.errorMessage = action.error.message;
+      if (action.payload === 'Unauthorized') {
+        state.isAuthenticated = false;
+        state.errorMessage = '認証に失敗しました。';
+      } else {
+        state.errorMessage = action.error.message;
+      }
+      state.isAuthenticated = false;
     });
   },
 });
 
 export default userSlice.reducer;
+export const { signin, logout } = userSlice.actions;
