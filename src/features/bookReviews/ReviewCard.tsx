2715a488795debcd2ced04389e25451ae5849349
yukinko05 <grey.sora.35@gmail.com>
2024-10-19 21:43:56 +0900

    一覧画面から詳細画面を開く再にログを送信する

--- src/features/bookReviews/ReviewCard.tsx
+++ src/features/bookReviews/ReviewCard.tsx
@@ -1,3 +1,6 @@
+import axiosInstance from '../../api/axiosInstance';
+import { useCookies } from 'react-cookie';
+
 import { Book } from '../../types';
 import { Link } from 'react-router-dom';
 interface ReviewCardProps {
@@ -7,6 +10,21 @@ interface ReviewCardProps {
 const ReviewCard = (props: ReviewCardProps) => {
   const { book } = props;
 
+  const [cookies] = useCookies(['token']);
+  const token = cookies.token;
+
+  const handleClickDetail = () => {
+    const data = {
+      selectBookId: book.id,
+    };
+
+    axiosInstance.post('/logs', data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
   return (
     <li className="border border-gray-300 rounded-md py-3 px-2 hover:bg-slate-200 bg-gray-50">
       <div className="border-b border-gray-300 py-2">
@@ -15,7 +33,11 @@ const ReviewCard = (props: ReviewCardProps) => {
       <p className=" px-2 py-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
       <p className="px-2 pt-2 text-sm mt-1 text-gray-900 truncate">{book.review}</p>
       <div className="flex justify-stretch text-center mt-4">
-        <Link to={`/detail/${book.id}`} target="_blank" className="w-full text-md text-gray-900">
+        <Link
+          to={`/detail/${book.id}`}
+          onClick={handleClickDetail}
+          className="w-full text-md text-gray-900"
+        >
           詳細
         </Link>
 
