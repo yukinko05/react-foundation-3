40357a6c892a3b0c8ea4493b023690954e2f577d
yukinko05 <grey.sora.35@gmail.com>
2024-10-19 19:12:23 +0900

    ルーティング処理と書籍レビューカードコンポーネントに編集画面のリンクを追加

--- src/features/bookReviews/ReviewCard.tsx
+++ src/features/bookReviews/ReviewCard.tsx
@@ -9,13 +9,22 @@ const ReviewCard = (props: ReviewCardProps) => {
 
   return (
     <li className="border border-gray-300 rounded-md py-3 px-2 hover:bg-slate-200 bg-gray-50">
-      <Link to={`/detail/${book.id}`} target="_blank" className="text-xl text-gray-900">
-        <div className="border-b border-gray-300 py-2">
-          <p className="line-clamp-2">{book.title}</p>
-        </div>
-        <p className=" px-2 py-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
-        <p className="px-2 pt-2 text-sm mt-1 text-gray-900 truncate">{book.review}</p>
-      </Link>
+      <div className="border-b border-gray-300 py-2">
+        <p className="line-clamp-2">{book.title}</p>
+      </div>
+      <p className=" px-2 py-3 text-sm text-gray-900">レビュワー：{book.reviewer}</p>
+      <p className="px-2 pt-2 text-sm mt-1 text-gray-900 truncate">{book.review}</p>
+      <div className="flex justify-stretch text-center mt-4">
+        <Link to={`/detail/${book.id}`} target="_blank" className="w-full text-md text-gray-900">
+          詳細
+        </Link>
+
+        {book.isMine && (
+          <Link to={`/edit/${book.id}`} target="_blank" className="w-full text-md text-gray-900">
+            編集
+          </Link>
+        )}
+      </div>
     </li>
   );
 };
